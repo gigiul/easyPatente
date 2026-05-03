6a38a0acfca8a00395f9a5096a704f1aa69cb486
Gigiul <luigidalleaste@gmail.com>
2026-05-03 23:42:16 +0200

    new translations

--- i18n/index.ts
+++ i18n/index.ts
@@ -1,25 +1,35 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
+import ar from './locales/ar.json';
 import bn from './locales/bn.json';
+import de from './locales/de.json';
 import en from './locales/en.json';
 import es from './locales/es.json';
+import fr from './locales/fr.json';
 import it from './locales/it.json';
+import ja from './locales/ja.json';
+import ko from './locales/ko.json';
+import pt from './locales/pt.json';
+import ru from './locales/ru.json';
+import si from './locales/si.json';
+import zh from './locales/zh.json';
 
 
 const resources = {
-  en: {
-    translation: en,
-  },
-  es: {
-    translation: es,
-  },
-  it: {
-    translation: it,
-  },
-  bn: {
-    translation: bn,
-  },
+  en: { translation: en },
+  it: { translation: it },
+  es: { translation: es },
+  bn: { translation: bn },
+  fr: { translation: fr },
+  de: { translation: de },
+  ar: { translation: ar },
+  zh: { translation: zh },
+  ja: { translation: ja },
+  ko: { translation: ko },
+  pt: { translation: pt },
+  ru: { translation: ru },
+  si: { translation: si },
 };
 
 i18n
