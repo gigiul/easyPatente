5a2f48780cb4efbfa954d329369d14028d2bdf55
Gigiul <luigidalleaste@gmail.com>
2026-07-14 20:57:01 +0200

    v1.0.5

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: 'Quiz Patente 2026',
   slug: 'easyPatente',
   description: 'Quiz patente multilingua',
-  version: '1.0.4',
+  version: '1.0.5',
   orientation: 'portrait',
   icon: './assets/images/IOSquizpatente.png',
   scheme: 'easypatente',
